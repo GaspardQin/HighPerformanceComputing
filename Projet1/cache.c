28c481e8d1be790f8a1247e91d4c6f244a40a9eb
Gaspard QIN <gaspard.qin@gmail.com>
2018-03-27 01:08:23 +0200

    finished partie1

--- Projet1/cache.c
+++ Projet1/cache.c
@@ -2,29 +2,33 @@
 
 /* initialization functions of vector/matrices with random and zero values */
 static void data_randominit(int n, double *A);
-static void data_zeroinit(int n, double *A); 
+static void data_zeroinit(int n, double *A);
 
 
 Cache * cache_init(int size, int strategy)
-{ 
+{
   int i;
   Cache *c = malloc(sizeof(Cache));
   if ( ! c ) return NULL;
   c->hits   = 0;
   c->misses = 0;
-  
+
   c->strategy   = strategy ;
   c->nbrblocks  = size;
   c->blocks     = malloc(sizeof(void *) * size );
   for(i=0; i<c->nbrblocks; i++) c->blocks[i] = NULL;
-  
+
   c->used     = malloc(sizeof(int) * size );
   for(i=0; i<c->nbrblocks; i++) c->used[i] = 0;
-  
+
+
+  c->lastUse   = malloc(sizeof(int) * size );
+  for(i=0; i<c->nbrblocks; i++) c->lastUse[i] = 0;
+
   c->accesstime = 0;
-  
+
   return c;
-  
+
 }
 
 Cache *  cache_free(Cache *c)
@@ -32,12 +36,12 @@ Cache *  cache_free(Cache *c)
   if ( !c )           return NULL;
   if ( c->blocks   )  free(c->blocks);
   free(c);
-  
+
   return NULL;
-  
+
 }
 
-int cache_isvalid(Cache *c ) 
+int cache_isvalid(Cache *c )
 {
   if ( ! c )          return 0;
   if ( ! c->blocks )  return 0;
@@ -50,15 +54,15 @@ int cache_isvalid(Cache *c )
 void cache_print(Cache *c, int level)
 {
   int i;
-  
+
   if ( ! cache_isvalid(c) ) return;
-  
+
   float hits_ratio = 100.0*(float)  c->hits  / (float) (c->hits+c->misses);
-  float mean_time  = (float) c->accesstime / (float) (c->hits+c->misses);
-  
-  printf(" ----- current cache cache size %d strategy %d hits %d misses %d hits-ratio %3.3f %%  Mean acces time %lg   \n",
-        c->nbrblocks, c->strategy, 
-        c->hits,c->misses, hits_ratio, mean_time); 
+  double mean_time  = (double) c->accesstime / (double) (c->hits+c->misses);
+
+  printf(" ----- current cache cache size %d strategy %d hits %d misses %d hits-ratio %3.3f %%  Mean acces time %f   \n",
+        c->nbrblocks, c->strategy,
+        c->hits,c->misses, hits_ratio, mean_time);
 
   if ( level == 1 ) {
     for(i=0; i<c->nbrblocks; i++) {
@@ -66,24 +70,25 @@ void cache_print(Cache *c, int level)
       //printf("%zu\n", sizeof(c->blocks[i]));
     }
   }
-   
+
 }
 
 
 void    cache_reset(Cache *c)
 {
-  int i; 
+  int i;
   if ( ! cache_isvalid(c) ) return;
-  
+
   c->hits   = 0;
   c->misses = 0;
-  
+
   for(i=0; i<c->nbrblocks; i++) c->blocks[i] = NULL;
   for(i=0; i<c->nbrblocks; i++) c->used[i]   = 0;
-  
+  for(i=0; i<c->nbrblocks; i++) c->lastUse[i]   = 0;
+
   c->accesstime = 0;
-  
-  
+
+
 }
 
 static unsigned int cache_hashkey(char *key, int len, int siz){
@@ -109,11 +114,11 @@ int my_cache_hashkey(void *adr, Cache* c){
 void cache_fetchmemory(Cache *c, void *adr)
 {
   int i;
-  
+
   if ( ! cache_isvalid(c) ) return;
-  
-  
-  
+
+
+
   if ( c->strategy == CACHE_DIRECT_MAPPING ) {
     //--- Direct mapping
     //int code = (long long int)(adr)  & (c->nbrblocks - 1);
@@ -122,16 +127,16 @@ void cache_fetchmemory(Cache *c, void *adr)
     //printf("%d \n", code);
     if (code >= c->nbrblocks )
         printf("out of index!!!");
-    if ( c->blocks[code] == adr ) { 
-      c->hits++;  c->used[code]   = 0; 
+    if ( c->blocks[code] == adr ) {
+      c->hits++;  c->used[code]   = 0;
     }
     else  {
       c->blocks[code] = adr;
       c->used[code]   = 1;
       c->misses++;
     }
-    
-    c->accesstime += 1; 
+
+    c->accesstime += 1;
     return ;
   }
   else if ( c->strategy == CACHE_FULLYASSOCIATIVE ) {
@@ -142,42 +147,84 @@ void cache_fetchmemory(Cache *c, void *adr)
       if ( c->blocks[i] == adr ) {
          c->used[i]++;
          c->hits++;
-         return; 
+         c->lastUse[i] = c->accesstime;
+         return;
       }
-      if ( c->used[i] < iMin ) {
-        iMin  = c->used[i];
-        iCode = i; 
+      if ( c->lastUse[i] < iMin ) {
+        iMin  = c->lastUse[i];
+        iCode = i;
       }
     }
-    
+
     c->blocks[iCode] = adr;
     c->used[iCode] = 1;
+    c->lastUse[iCode] = c->accesstime;
     c->misses++;
     return;
   }
-  else if ( c->strategy == CACHE_2WAYASSOCIATIVE ) { 
-    printf(" cache : strategy CACHE_2WAYASSOCIATIVE to do ! \n");
-    exit(1); 
+  else if ( c->strategy == CACHE_2WAYASSOCIATIVE ) {
+    int iMinTime = INT_MAX;
+    int code = cache_hashkey((char*)adr,8,c->nbrblocks/2);
+    int iCode  = 0;
+    for(i=0;i<2;i++){
+      c->accesstime ++;
+      int asso_index = i*(int)((c->nbrblocks)/2)+code;
+      if(c->blocks[asso_index] == adr){
+        c->used[asso_index] ++;
+        c->hits++;
+        c->lastUse[asso_index] = c->accesstime;
+        return;
+      }
+      if(c->lastUse[asso_index]<iMinTime){
+        iMinTime = c->lastUse[asso_index];
+        iCode = asso_index;
+      }
+    }
+    c->blocks[iCode] = adr;
+    c->used[iCode] = 1;
+    c->lastUse[iCode] = c->accesstime;
+    c->misses++;
+    return;
   }
-  else if ( c->strategy == CACHE_4WAYASSOCIATIVE ) { 
-    printf(" cache : strategy CACHE_4WAYASSOCIATIVE to do ! \n"); 
-    exit(1); 
+  else if ( c->strategy == CACHE_4WAYASSOCIATIVE ) {
+    int iMinTime = INT_MAX;
+    int code = cache_hashkey((char*)adr,8,c->nbrblocks/4);
+    int iCode  = 0;
+    for(i=0;i<4;i++){
+      c->accesstime ++;
+      int asso_index = i*(int)((c->nbrblocks)/4)+code;
+      if(c->blocks[asso_index] == adr){
+        c->used[asso_index] ++;
+        c->hits++;
+        c->lastUse[asso_index] = c->accesstime;
+        return;
+      }
+      if(c->lastUse[asso_index]<iMinTime){
+        iMinTime = c->lastUse[asso_index];
+        iCode = asso_index;
+      }
+    }
+    c->blocks[iCode] = adr;
+    c->used[iCode] = 1;
+    c->lastUse[iCode] = c->accesstime;
+    c->misses++;
+    return;
   }
   else  {
     printf(" cache : invalid cache strategy souuld be one of :  CACHE_DIRECT_MAPPING CACHE_FULLYASSOCIATIVE CACHE_2WAYASSOCIATIVE CACHE_4WAYASSOCIATIVE \n");
     exit(1);
   }
-    
-} 
+
+}
 
 
-static void data_randominit(int n, double *A) 
+static void data_randominit(int n, double *A)
 {
    int i;
    for(i=0; i<n; i++) A[i] = (float)rand()/(float)(RAND_MAX);
 }
-  
-static void data_zeroinit(int n, double *A) 
+
+static void data_zeroinit(int n, double *A)
 {
    int i;
    for(i=0; i<n; i++) A[i] = 0.0;
@@ -188,12 +235,12 @@ void cache_dotprod_block(int n, Cache *c, int blocksize)
 {
   int i;
   double dot = 0;
-  
+
   double A[n];
   double B[n];
   int num_double_in_block = blocksize / 8;
   if ( ! cache_isvalid(c) ) return;
-  
+
   data_randominit(n, A);
   data_randominit(n, B);
   int bloc_cached_index ;
@@ -202,40 +249,40 @@ void cache_dotprod_block(int n, Cache *c, int blocksize)
     cache_fetchmemory(c, (void *) &(A[bloc_cached_index]));
     cache_fetchmemory(c, (void *) &(B[bloc_cached_index]));
     dot = dot + A[i]*B[i];
-    printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);  
+    //printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);
   }
-  printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);  
-}  
+  printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);
+}
 
 void cache_dotprod(int n, Cache *c)
 {
   int i;
   double dot = 0;
-  
+
   double A[n];
   double B[n];
   if ( ! cache_isvalid(c) ) return;
-  
+
   data_randominit(n, A);
   data_randominit(n, B);
-  
+
   for(i=0; i<n; i++) {
     cache_fetchmemory(c, (void *) &(A[i]));
     cache_fetchmemory(c, (void *) &(B[i]));
     dot = dot + A[i]*B[i];
   }
-  printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);  
-}  
-  
+  printf(" dot = %lg hits %d misses %d \n",dot,c->hits, c->misses);
+}
+
 void cache_matvec(int n, Cache *c)
 {
   int i,j;
   double A[n][n];
   double B[n];
   double AB[n];
-  
+
   if ( ! cache_isvalid(c) ) return;
- 
+
   data_randominit(n*n, (double *) A);
   data_randominit(n, B);
   data_zeroinit(  n, AB);
@@ -246,15 +293,16 @@ void cache_matvec(int n, Cache *c)
       AB[i] = AB[i] + A[i][j]*B[j];
     }
   }
-  printf("  hits %d misses %d \n",c->hits, c->misses);  
+  printf("  hits %d misses %d \n",c->hits, c->misses);
 }
 void cache_matvec_block(int n, Cache *c, int blocksize)
 {
+
   int i,j;
   double A[n][n];
   double B[n];
   double AB[n];
-  
+
   if ( ! cache_isvalid(c) ) return;
   int num_double_in_block = blocksize / 8;
   data_randominit(n*n, (double *) A);
@@ -269,16 +317,54 @@ void cache_matvec_block(int n, Cache *c, int blocksize)
       AB[i] = AB[i] + A[i][j]*B[j];
     }
   }
-  printf("  hits %d misses %d \n",c->hits, c->misses);  
+  printf("  hits %d misses %d \n",c->hits, c->misses);
 }
 
-void cache_matmat(int n, Cache *c)
+void cache_matmat_block(int n, Cache *c, int blocksize, int mat_block_coeffi)
 {
+    //mat_block_coeffi means the matrix block's width is (mat_block_coeffi * num_double_in_block)
   if ( ! cache_isvalid(c) ) return;
-  
-  printf("  cache_matmat :  not implemented \n");
-  exit(1);
-  
+  int i_A,j_A,j_B;
+  int i_block_A, j_block_A, j_block_B;
+  double A[n][n];
+  double B[n][n];
+  double AB[n][n];
+
+  int num_double_in_block = blocksize / 8;
+  data_randominit(n*n, (double *) A);
+  data_randominit(n*n, (double *) B);
+  data_zeroinit(n*n, (double *)AB);
+  int mat_block_width = mat_block_coeffi *  num_double_in_block;
+  for(i_block_A=0; i_block_A<n; i_block_A+=mat_block_width) { //num_double_in_block lines for each iterate
+    for(j_block_A = 0; j_block_A < n; j_block_A += mat_block_width ){
+
+      //in block
+      for(j_B = 0; j_B<n; j_B ++){
+
+        for(i_A = i_block_A; i_A<mat_block_width + i_block_A;i_A++){
+          j_block_B = j_B % mat_block_width;
+          int temp = 0;
+          for(j_A =j_block_A; j_A<mat_block_width + j_block_A; j_A++) {
+            cache_fetchmemory(c, (void *) &(A[i_A][(int)(j_A/num_double_in_block)]));
+
+            cache_fetchmemory(c, (void *) &(B[j_A][(int)(j_B/num_double_in_block)]));
+
+            temp += A[i_A][j_A] * B[j_A][j_B];
+          }
+          cache_fetchmemory(c, (void *) &(AB[i_A][(int)(j_B/num_double_in_block)])); //also compte the access of AB matrix
+          AB[i_A][j_B] += temp;
+
+        }
+
+      }
+    }
+    //printf("write AB line %d\n", i_block_A);
+  }
+  printf("Matrix prod Marix  hits %d misses %d \n",c->hits, c->misses);
+
+  //printf("  cache_matmat :  not implemented \n");
+  //exit(1);
+
 }
 
 
@@ -286,22 +372,27 @@ void cache_test()
 {
 
   int size      = 256;
-  int strategy  = CACHE_FULLYASSOCIATIVE;
+  //int strategy  = CACHE_FULLYASSOCIATIVE;
   //int strategy  = CACHE_DIRECT_MAPPING;
-  
+  //int strategy  = CACHE_2WAYASSOCIATIVE;
+  int strategy = CACHE_4WAYASSOCIATIVE;
   Cache * c = cache_init(size, strategy);
-  
+
+
+  //cache_dotprod(1024 ,c);
   cache_dotprod_block(1024 ,c,32);
-  cache_print(c, 1);
+  cache_print(c, 0);
   cache_reset(c);
-  
+
   //cache_matvec ( 512 ,c);
   cache_matvec_block(512,c, 32);
-  cache_print(c, 1);
-  
-  cache_reset(c); 
-  
+  cache_print(c, 0);
+  cache_reset(c);
+
+  //mat matrix
+
+  cache_matmat_block(512, c, 32, 2);
+  cache_print(c, 0);
+  cache_reset(c);
+
 }
-
-
-
--- Projet1/cache.h
+++ Projet1/cache.h
@@ -1,13 +1,13 @@
 /*
-  
-  Simulating a very simple cache to estimate the number of 
-  misses and hits  
-  
-  
+
+  Simulating a very simple cache to estimate the number of
+  misses and hits
+
+
   The principal functions to modify are :
     cache_fetchmemory(Cache *c, void *adr);
-    cache_test();       
-  
+    cache_test();
+
 */
 
 
@@ -26,16 +26,16 @@ typedef struct t_cache
 {
   int hits;
   int misses;
-  
+
   int strategy;
   int nbrblocks;
   void **blocks; /* adress */
   int   *used;   /* store how many time the data was fetched */
-  
-  int accesstime; /* sum up access times to retrieve data (number of tests)*/
-  
+  int *lastUse; //last time used this block
+  int64_t accesstime; /* sum up access times to retrieve data (number of tests)*/
+
 } Cache;
- 
+
 
 Cache * cache_init(int size, int strategy);
 Cache * cache_free(Cache *c);
@@ -43,13 +43,13 @@ void    cache_reset(Cache *c);                  /* Reset all variables and remov
 int     cache_isvalid(Cache *c );               /* Verify that all arrays are properly allocated in stacks            */
 void    cache_print(Cache *c, int level);       /* Print the cache, if Level > 1 all the stored adresses are reported */
 void    cache_fetchmemory(Cache *c, void *adr); /* get an address and set the number of hits/misses, time of access   */
- 
 
-/* 
 
-  Simple functions to evaluate the efficiency of the cache approach 
-  Warning : for simplicity, arrays/matrices are allocated on stack, 
-  to make experiments with large matrix, write function to dynalically allocate vectors/matrices 
+/*
+
+  Simple functions to evaluate the efficiency of the cache approach
+  Warning : for simplicity, arrays/matrices are allocated on stack,
+  to make experiments with large matrix, write function to dynalically allocate vectors/matrices
 
 */
 void cache_dotprod(int n, Cache *c);
@@ -58,4 +58,3 @@ void cache_matmat( int n, Cache *c);
 void cache_dotprod_block(int n, Cache *c, int blocksize);
 void cache_matvec_block(int n, Cache *c, int blocksize);
 void cache_test(void);   /* main functions of tests */
-
